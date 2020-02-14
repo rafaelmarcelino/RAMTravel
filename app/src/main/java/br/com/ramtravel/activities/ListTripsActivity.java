3d502aa649a1655c58861fdcba7b318be104b881
Rafael Alves Marcelino <ram.programacao@gmail.com>
2020-02-14 14:23:40 -0300

    Initialized project RAM Travel app

---
+++ app/src/main/java/br/com/ramtravel/activities/ListTripsActivity.java
@@ -0,0 +1,16 @@
+package br.com.ramtravel.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import br.com.ramtravel.R;
+
+public class ListTripsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_trips);
+    }
+}
