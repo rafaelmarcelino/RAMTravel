6381080ad0c42fdf0dec2699645971704bc0fd86
Rafael Alves Marcelino <ram.programacao@gmail.com>
2020-02-20 20:29:19 -0300

    Added cardview and list view

--- app/src/main/java/br/com/ramtravel/activities/ListTripsActivity.java
+++ app/src/main/java/br/com/ramtravel/activities/ListTripsActivity.java
@@ -8,9 +8,12 @@ import br.com.ramtravel.R;
 
 public class ListTripsActivity extends AppCompatActivity {
 
+    private static final String LIST_TRIPS = "List Trips";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_trips);
+        setTitle(LIST_TRIPS);
     }
 }
